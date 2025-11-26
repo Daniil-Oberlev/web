c595ab55e5b2df5568fe30a83806358bab9a4111
Daniil Oberlev <denioberlev@gmail.com>
2025-11-26 16:46:45 +0300

    feat(pages): add site-policies

--- frontend/src/widgets/Footer/index.tsx
+++ frontend/src/widgets/Footer/index.tsx
@@ -1,11 +1,19 @@
 import './index.css';
 
+import { Link } from 'react-router-dom';
+
+import { ROUTES } from '@/shared/api/routes';
+
 import { getCopyright } from './copyright';
 
 export const Footer = () => {
   return (
     <footer className='footer'>
       <p>{getCopyright()}</p>
+      <nav>
+        <Link to={ROUTES.PRIVACY}>Политика конфиденциальности</Link> |{' '}
+        <Link to={ROUTES.TERMS}>Пользовательское соглашение</Link>
+      </nav>
     </footer>
   );
 };
